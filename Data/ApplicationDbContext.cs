2edb8b5f52b35667a59c70e6126771b49a58e78d
Vinicius Araújo <viniciushsaraujoo@gmail.com>
2020-01-14 14:04:24 -0300

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CRUD_com_banco_de_dados.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_com_banco_de_dados.Data {
+    public class ApplicationDbContext : DbContext {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {
+        }
+
+        public DbSet<Pessoa> Pessoas { get; set; }
+
+    }
+}
---
+++ Repositories/PessoaRepository.cs
@@ -0,0 +1,43 @@
+﻿using CRUD_com_banco_de_dados.Data;
+using CRUD_com_banco_de_dados.Models;
+using CRUD_com_banco_de_dados.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_com_banco_de_dados.Repositories {
+    public class PessoaRepository : IPessoaRepository {
+
+        private readonly ApplicationDbContext _dbContext;
+
+        public PessoaRepository(ApplicationDbContext dbContext) {
+            _dbContext = dbContext;
+        }
+
+        public Pessoa Buscar(int id) {
+            return _dbContext.Pessoas.FirstOrDefault(p => p.Id == id);
+        }
+
+        public void Cadastrar(Pessoa pessoa) {
+            _dbContext.Add(pessoa);
+            _dbContext.SaveChanges();
+        }
+
+        public void Editar(Pessoa pessoa) {
+             _dbContext.Update(pessoa);
+            _dbContext.SaveChanges();
+        }
+
+        public void Excluir(int id) {
+            var pessoa = Buscar(id);
+            _dbContext.Remove(pessoa);
+            _dbContext.SaveChanges();
+        }
+
+        public List<Pessoa> Listar() {
+            return _dbContext.Pessoas.ToList();
+        }
+    }
+}
